b44e52672d3f54fadfd9b1bc9b1f715ab8bbcdd9
mariapascuallozano <mariapascuallozano@gmail.com>
2023-03-23 10:37:40 +0000

    Grid profile

--- scripts/news.js
+++ scripts/news.js
@@ -3,8 +3,9 @@ let port = 3002;
 document.addEventListener("DOMContentLoaded", () => {
   const searchParams = new URLSearchParams(window.location.search);
   const id = searchParams.get("id");
+  const port = 3002;
 
-  fetch("http://localhost:" + port + "/news")
+  fetch(`http://localhost:${port}/news`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -16,19 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(error => {
       console.error(error);
+    })
+  fetch(`http://localhost:${port}/news`)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(newsItem => {
+        const button = document.querySelector(`[data-id="${newsItem.id}"]`);
+        const h1 = button.querySelector('h1');
+        h1.innerText = newsItem.title;
+      });
+    })
+    .catch(error => {
+      console.error(error);
     });
 });
-
-fetch("http://localhost:" + port + "/news")
-  .then(response => response.json())
-  .then(data => {
-    // Iteramos sobre cada objeto del JSON
-    data.forEach(newsItem => {
-      // Obtenemos el botón correspondiente por su data-id
-      const button = document.querySelector(`[data-id="${newsItem.id}"]`);
-      // Obtenemos la etiqueta h1 del botón
-      const h1 = button.querySelector('h1');
-      // Asignamos el título del objeto JSON a la etiqueta h1
-      h1.innerText = newsItem.title;
-    });
-  });
